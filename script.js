10cd5ccfca0cac19ad037b975db71bb245ce1788
Mathiex <72783352+Mathiex@users.noreply.github.com>
2020-11-12 09:31:27 +0100

    video fixed

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function RandalTunnelDone(){
+/*function RandalTunnelDone(){
     setTimeout('',5000)
     var x = document.getElementById("randalDiv")
     x.style.display = "none"
+}*/
+/*function RandalTunnelDone(){
+    var x = document.getElementById("tunnelRandall")
+    x.hidden(true)
+}*/
+function RandalTunnelDone(){
+    document.getElementById("tunnelRandall").style.display = "none";
+
 }
