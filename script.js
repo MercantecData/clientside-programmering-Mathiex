4da005c0ddd3377dae3ac3c46e6fa0ae589a3f51
Mathiex <72783352+Mathiex@users.noreply.github.com>
2020-11-12 11:02:22 +0100

    filling out the blanks

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function RandalTunnelDone(){
     document.getElementById("tunnelRandall").style.display = "none";
 
 }
+
+src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&libraries=&v=weekly"
+      defer
+
+function initMap() {
+
+    const uluru = { lat: -25.344, lng: 131.036 };
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
